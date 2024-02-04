4413d4a0bc51f1b6d00b2b4242e72596d6181f8e
Rohan Anil Tade <114643307+rohantade8@users.noreply.github.com>
2024-02-05 02:23:48 +0530

    Add files via upload

---
+++ 010_operators.cpp
@@ -0,0 +1,155 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int a=10;
+//     cout<<a++;
+//     cout<<endl<<a;
+// }
+
+
+// {
+//     int a=10;
+//     int b=a++;
+//     cout<<b<<" "<<a;
+// }
+
+// {
+//      int a=10;
+//      int b=a--;
+//      cout<<b<<" "<<a;
+// }
+
+
+// {
+//     //comparison operator
+//     // 1 or 0
+//     int a,b;
+//     cout<<"enter the value of a: ";
+//     cin>>a;
+//     cout<<"enter the value of b: ";
+//     cin>>b;
+
+//   ==
+    // if (a==b)
+    // {
+    //     cout<<"yes";
+    // }
+    // else{
+    //     cout<<"no";
+    // }
+
+// >
+//     if(a>b)
+//     cout<<"yes";
+//     else
+//     cout<<"no";
+
+
+// <
+//     if(a<b)
+//     cout<<"yes";
+//     else
+//     cout<<"no";   
+
+// >=
+//     if(a>=b)
+//     cout<<"yes";
+//     else
+//     cout<<"no";
+
+// <=
+    // if(a<=b)
+    // cout<<"yes";
+    // else
+    // cout<<"no";
+
+// !=
+//     if(a!=b)
+//     cout<<"yes";
+//     else
+//     cout<<"no";
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+    // Logical Operator
+    // AND operator
+    // int a,b,c;
+    // cin>>a>>b>>c;
+
+    // if(a>b&&a>c)
+    // cout<<"yes";
+    // else
+    // cout<<"no";
+
+
+
+    // OR operator
+    // char name = 'a';
+
+    // if(name=='a'||name=='e' ||name=='i'||name=='o'||name=='u')
+    // cout<<"Vowel";
+    // else
+    // cout<<"consonent";
+
+
+//     cout<<!0<<endl;
+
+// }
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //Bitwise Operator
+
+    // & Operator
+    // int ans = 2&3;
+    // cout<<ans<<endl;
+
+    // | Operator
+    // int ans = 2|3;
+    // cout<<ans<<endl;
+
+    // ^ Operator
+    // int ans = 2^3;
+    // cout<<"My answer is: "<<ans<<endl;
+
+    // << Operator
+    // int ans = 2<<3;
+    // cout<<"My answer is: "<<ans<<endl;
+
+    // >> Operator
+    // int ans = 2>>3;
+    // cout<<"My answer is: "<<ans<<endl;
+
+    // >> Operator
+    // int ans = 18>>3;
+    // cout<<"My answer is: "<<ans<<endl;
+
+    // ~ Operator
+    // int ans = ~10 ;
+    // cout<<"My answer is: "<<ans<<endl;
+
+    // // ~ Operator
+    // int ans = ~-10;
+    // cout<<"My answer is: "<<ans<<endl;
+
+
+    // Assignment Operator
+
+    // int a =10;
+    // a-=15;  //a = a+15;
+    // cout<<a;
+
+    if(8>5>4>3<2*4)
+    cout<<"Yes";
+    else
+    cout<<"No";
+}
